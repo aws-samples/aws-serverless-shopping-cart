f82c2aa3c110e5445630ce2dda09c4e36a1469e8
Divz2725 <divz@amazon.com>
2022-09-29 19:10:06 +0530

    im

--- frontend/src/components/Product.vue
+++ frontend/src/components/Product.vue
@@ -12,7 +12,8 @@
     </v-row>
     <v-card-text class="pt-0 pl-4 pb-0">
       <p class="pt-0 pb-0 mb-0 body-2">"{{product.description}}"</p>
-      <img src=" {{product.pictures[0]}}"/>
+      <!---<img src=" {{product.pictures[0]}}"/>-->
+      <img src="https://d18mx5gr4fiw2a.cloudfront.net/LED_streetlight.png"/>
     
       <p class="price pt-0 pb-0 grow accent--text mb-1">${{getPrice(product)}}</p>
     </v-card-text>
